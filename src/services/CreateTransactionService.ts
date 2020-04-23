5ca11e8bda958be29c89499fcbdb9c026fb63873
Igor <ruas.igor@gmail.com>
2020-04-23 18:11:34 -0300

    avançando nas funcionalidades

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,49 @@
-// import AppError from '../errors/AppError';
-
+import { getRepository, getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import Categoy from '../models/Category';
 import Transaction from '../models/Transaction';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  public async execute({
+    title,
+    value,
+    type,
+    category,
+  }: Request): Promise<Transaction> {
+    const transationsRepository = getCustomRepository(TransactionsRepository);
+    const categoryRepository = getRepository(Categoy);
+    let categoryExits = await categoryRepository.findOne({
+      where: { title: category },
+    });
+
+    if (!categoryExits) {
+      categoryExits = categoryRepository.create({ title: category });
+      await categoryRepository.save(categoryExits);
+    }
+
+    const balance = await transationsRepository.getBalance();
+
+    if (type === 'outcome' && balance.total < value) {
+      throw new AppError('Transaction invalid! Outcome > total');
+    }
+    if (type !== 'income' && type !== 'outcome') {
+      throw new AppError('Type invalid!');
+    }
+    const transaction = await transationsRepository.create({
+      title,
+      value,
+      type,
+      category: categoryExits,
+    });
+    await transationsRepository.save(transaction);
+    return transaction;
   }
 }
 
--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,19 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
 
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+
+interface Request {
+  id: string;
+}
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute({ id }: Request): Promise<void> {
+    const transationsRepository = getCustomRepository(TransactionsRepository);
+    const transaction = await transationsRepository.findOne(id);
+    if (!transaction) {
+      throw new AppError('Transaction does not exits.');
+    }
+    await transationsRepository.remove(transaction);
   }
 }
 
